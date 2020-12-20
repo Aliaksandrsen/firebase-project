372647949cb2ce1536d026340e62f5f696685216
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-12-20 18:00:35 +0300

    testing firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useCallback } from "react";
+import firebase from "firebase/app";
 
-function App() {
+import "./App.css";
+
+export const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [key, setKey] = useState("");
+  const [value, setValue] = useState("");
+
+  const [hasAccount, setHasAccount] = useState(false);
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    const db = firebase.database();
+
+    const name = db.ref("name");
+    name.on("value", (elem) => {
+      console.log(elem.val());
+      setName(elem.val());
+      return elem.val();
+    });
+  }, []);
+
+  const onChangeEmail = useCallback(({ target: { value } }) => {
+    setEmail(value);
+  }, []);
+
+  const onChangePassword = useCallback(({ target: { value } }) => {
+    setPassword(value);
+  }, []);
+
+  const onChangeKey = useCallback(({ target: { value } }) => {
+    setKey(value);
+  }, []);
+
+  const onChangeValue = useCallback(({ target: { value } }) => {
+    setValue(value);
+  }, []);
+
+  const onSubmitData = useCallback(() => {
+    const db = firebase.database();
+
+    db.ref(key).push(value);
+    alert("your data was written to db");
+  }, [key, value]);
+
+  const onSubmitAuth = useCallback(() => {
+    // create auth
+
+    // firebase
+    //   .auth()
+    //   .createUserWithEmailAndPassword(email, password)
+    //   .catch((err) => console.log(err));
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((res) => setHasAccount(true))
+      .catch((err) => console.log(err));
+  }, [email, password]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {hasAccount ? (
+        <>
+          <div>Hello</div>
+          {name && <div>{name}</div>}
+
+          <input
+            type="text"
+            placeholder="Key"
+            value={key}
+            onChange={onChangeKey}
+          />
+
+          <input
+            type="text"
+            placeholder="Value"
+            value={value}
+            onChange={onChangeValue}
+          />
+
+          <input type="submit" onClick={onSubmitData} />
+        </>
+      ) : (
+        <>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={onChangeEmail}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChangePassword}
+          />
+
+          <input type="submit" onClick={onSubmitAuth} />
+        </>
+      )}
     </div>
   );
-}
-
-export default App;
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase";
+
+import { App } from "./App";
+
+import "./index.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjfvcd-qoER223SmUOFFkAxhcXEddeLhM",
+  authDomain: "fir-test-37b18.firebaseapp.com",
+  databaseURL: "https://fir-test-37b18-default-rtdb.firebaseio.com",
+  projectId: "fir-test-37b18",
+  storageBucket: "fir-test-37b18.appspot.com",
+  messagingSenderId: "897594249618",
+  appId: "1:897594249618:web:a3af25415eb756300d314b",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
